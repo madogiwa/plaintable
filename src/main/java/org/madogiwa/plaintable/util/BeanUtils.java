eda4803e1078592d68ef84b9bb0edc5befcc3910
Hidenori Sugiyama <madogiwa@gmail.com>
2013-01-16 03:15:15 +0900

    support primitive type in bean property.

--- src/main/java/org/madogiwa/plaintable/util/BeanUtils.java
+++ src/main/java/org/madogiwa/plaintable/util/BeanUtils.java
@@ -11,7 +11,25 @@ public class BeanUtils {
     public static void setBeanProperty(Object bean, Method setter, Object value) {
         try {
             Class parameterClass = setter.getParameterTypes()[0];
-            setter.invoke(bean, parameterClass.cast(value));
+            if (parameterClass.isAssignableFrom(boolean.class)) {
+                setter.invoke(bean, ((Boolean)value).booleanValue());
+            } else if (parameterClass.isAssignableFrom(byte.class)) {
+                setter.invoke(bean, ((Byte)value).byteValue());
+            } else if (parameterClass.isAssignableFrom(short.class)) {
+                setter.invoke(bean, ((Short)value).shortValue());
+            } else if (parameterClass.isAssignableFrom(char.class)) {
+                setter.invoke(bean, ((Character)value).charValue());
+            } else if (parameterClass.isAssignableFrom(int.class)) {
+                setter.invoke(bean, ((Integer)value).intValue());
+            } else if (parameterClass.isAssignableFrom(long.class)) {
+                setter.invoke(bean, ((Long)value).longValue());
+            } else if (parameterClass.isAssignableFrom(float.class)) {
+                setter.invoke(bean, ((Float)value).floatValue());
+            } else if (parameterClass.isAssignableFrom(double.class)) {
+                setter.invoke(bean, ((Double)value).doubleValue());
+            } else {
+                setter.invoke(bean, value);
+            }
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         } catch (InvocationTargetException e) {
